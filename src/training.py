49570de7877a986607b4935f8918ae565f32a421
SutirthaSahu <sutirthasahu@gmail.com>
2021-11-17 12:46:04 +0530

    added save plot functionality

--- src/training.py
+++ src/training.py
@@ -1,6 +1,6 @@
 from src.utils.common import read_config
 from  src.utils.data_mgmt import get_data
-from src.utils.model import create_model,save_model
+from src.utils.model import create_model,save_model,save_plot
 import argparse
 import os
 
@@ -26,13 +26,19 @@ def training(config_path):
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
-    
+    plots_dir = config["artifacts"]["plots_dir"]
+
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     os.makedirs(model_dir_path, exist_ok=True)
     
-    model_name = config["artifacts"]["model_name"]
+    plot_dir_path = os.path.join(artifacts_dir, plots_dir)
+    os.makedirs(plot_dir_path, exist_ok=True)
 
+    model_name = config["artifacts"]["model_name"]
+    plot_name = config["artifacts"]["plot_name"]
+    
     save_model(model, model_name, model_dir_path)
+    save_plot(history, plot_name, plot_dir_path)
 
 
 if __name__ == '__main__':
